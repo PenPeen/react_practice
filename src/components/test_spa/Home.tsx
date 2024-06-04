e648a3f18759a51c4e1511093cafd7de97fc0309
Pen Peen <ikegami1022.h@gmail.com>
2024-06-05 08:06:34 +0900

    :memo: searchParams検証用のコンポーネント作成

--- src/components/test_spa/Home.tsx
+++ src/components/test_spa/Home.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react';
 
+import { useSearchParams } from 'react-router-dom';
+
 const Home: FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const hogeValue = searchParams.get('hoge');
+  const updateSearchParams = () => {
+    setSearchParams({ hoge: 'hoge' });
+  };
+
   return (
     <>
       <h1>MyHome!</h1>
+      <div>
+        <button onClick={updateSearchParams}>updateHogeSearchParams</button>
+      </div>
+      <div>
+        <p>hoge: {hogeValue}</p>
+      </div>
     </>
   );
 };
