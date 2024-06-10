3e85cc2d3f893f577ce8d5782d392f6e8e637cb1
Pen Peen <ikegami1022.h@gmail.com>
2024-06-11 08:33:11 +0900

    :sparkles: 子コンポーネントでContextの取得

---
+++ src/components/test_use_context_2/Child.tsx
@@ -0,0 +1,17 @@
+import { FC, useContext } from 'react';
+
+import { ParentContext } from './Parent';
+
+const Child: FC = () => {
+  // 3. Context Valueの取得
+  const [state] = useContext(ParentContext);
+
+  return (
+    <>
+      <h2>Child</h2>
+      <p>state = {state}</p>
+    </>
+  );
+};
+
+export default Child;
