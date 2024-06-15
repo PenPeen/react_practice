e862e3abaeed9e263a4052b733d678a631642552
Pen Peen <ikegami1022.h@gmail.com>
2024-06-15 15:36:57 +0900

    :memo: 天気情報で使用するデータの定義

---
+++ src/components/test_effect_asynchronous/data/index.ts
@@ -0,0 +1,2 @@
+export * from './openMetroApi';
+export * from './weatherConditions';
---
+++ src/components/test_effect_asynchronous/data/openMetroApi.ts
@@ -0,0 +1,2 @@
+export const requestPath =
+  'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current=temperature_2m,weather_code&timezone=Asia%2FTokyo&forecast_days=1';
