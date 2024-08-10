7360c16ec3fdab655c62b5ac54cfa11de890ddab
Pen Peen <ikegami1022.h@gmail.com>
2024-08-10 11:15:57 +0900

    :memo: Stateの復習

---
+++ src/components/sample/Test0810.tsx
@@ -0,0 +1,39 @@
+import { FC, PropsWithChildren, useState } from 'react';
+
+type Props = {
+  name: string;
+};
+
+const initialData = ['data1', 'data2', 'data3'];
+
+const Test: FC<Props & PropsWithChildren> = ({ name }) => {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(initialData);
+  const countUp = () => setCount((count) => ++count);
+  const addData = () => setData((data) => [...data, 'addData']);
+  console.log('rendered');
+
+  return (
+    <>
+      <h1>Hello! {name}</h1>
+
+      <div>
+        <div>count</div>
+        <div>count = {count}</div>
+        <div>
+          <button onClick={countUp}>count up</button>
+        </div>
+      </div>
+
+      <div>
+        <div>data</div>
+        <div>data = {data.join(', ')}</div>
+        <div>
+          <button onClick={addData}>addData</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Test;
